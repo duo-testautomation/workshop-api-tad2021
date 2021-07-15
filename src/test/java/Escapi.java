10d4123e1f33b9d7492026d0464b7365a4196a63
Jurian Jilderda <jurian.jilderda@duo.nl>
2021-07-15 10:38:41 +0200

    first real commit

---
+++ src/test/java/Escapi.java
@@ -0,0 +1,20 @@
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+
+public class Escapi {
+
+
+    @Test
+    public void ecapeTheApi() {
+
+
+    }
+
+
+
+
+
+}
