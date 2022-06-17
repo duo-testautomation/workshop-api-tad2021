e0ec05b8328f62012f8ad76decc7c6e0f5a89319
Juryman <captain-awesome@outlook.com>
2022-06-17 08:22:10 +0200

    solution for the workshop

--- src/test/java/Escapi.java
+++ src/test/java/Escapi.java
@@ -1,49 +1,169 @@
 import io.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
+
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
 
 
 public class Escapi {
 
+    String apikey = "YOUR-API-KEY";
+    int sumSolution;
+    Towers towersSolution;
+    String solution1Header;
+    String solution2Header;
+    int specialistNumber;
+    int escapeCode;
 
-    //check the README for useful information about Rest Assured
+    @Before
+    public void setup() {
+        baseURI = "https://escape.ta-workshop.nl/duo";
+    }
 
     @Test
     public void ecapeTheApi() {
-
+        start();
+        towersGet();
+        combinationLock();
+        towersPost();
+        toolSupport();
+        escapeTheRoom();
     }
 
-
-    @Test
-    public void example(){
-        // sets a baseuri for every request in this run
-        baseURI = "https://randomuser.me";
-        Response exampleRequest =
+    private void start() {
+        Response startCallResponse =
                 given().
                         // logs the request in the console
-                        log().all().
-                        // adds a queryparam to the request
-                        queryParam("color", "red").
-                when().
+                                log().all().
+                        header("apikey", apikey).
+                        when().
                         // combines the baseURI with /api and makes a GET request
-                        get("/api").
-                then().
+                                get("/start").
+                        then().
                         // logs the response in the console
-                        log().all().
+                                log().all().
                         // validates that that the statuscode from the respons is 200
-                        statusCode(200).
+                                statusCode(201).
                         // checks if the body contains a gender node with value female
-                        body("results[0].color", equalTo("red")).
-                extract()
+                                extract()
                         // this will save the response in the exampleRequest variable
                         .response();
-        // extract a single value from the response
-        String lastName = exampleRequest.jsonPath().get("results[0].name.last");
+        // extract the needed values from the response
+        int number1 = startCallResponse.jsonPath().get("sum.number1");
+        int number2 = startCallResponse.jsonPath().get("sum.number2");
+        if (startCallResponse.jsonPath().get("divide")) {
+            sumSolution = number1 / number2;
+        } else if (startCallResponse.jsonPath().get("add")) {
+            sumSolution = number1 + number2;
+        } else if (startCallResponse.jsonPath().get("multiply")) {
+            sumSolution = number1 * number2;
+        } else {
+            sumSolution = number1 - number2;
+        }
         // prints output to the console
-        System.out.println("The lastname found in the extracted response = " + lastName);
+        System.out.println("The solution should be: " + sumSolution);
     }
 
+    private void towersGet() {
+        Response towersGetRespons =
+                given().
+                        log().all().
+                        header("apikey", apikey).
+                        when().
+                        get("/towers").
+                        then().
+                        log().all().
+                        statusCode(200).
+                        extract()
+                        .response();
+        // create tower answer
+        if (towersGetRespons.jsonPath().get("towers.alphabetic")) {
+            towersSolution = new Towers(1, 0, 2, 3, 4);
+        } else if (towersGetRespons.jsonPath().get("towers.height")) {
+            towersSolution = new Towers(3, 1, 4, 0, 2);
+        } else {
+            towersSolution = new Towers(1, 4, 0, 2, 3);
+        }
+    }
+
+    private void combinationLock() {
+        Response combinationLockRespons =
+                given().
+                        log().all().
+                        header("apikey", apikey).
+                        queryParam("solution", sumSolution).
+                 when().
+                        get("/combination_lock").
+                 then().
+                        log().all().
+                        statusCode(200).
+                 extract()
+                        .response();
+
+        solution1Header = combinationLockRespons.header("solution1");
+        System.out.println("The solutionHeader for puzzle 1: " + solution1Header);
+    }
+
+    private void towersPost() {
+        Response towersPostResponse =
+                given().
+                        log().all().
+                        header("apikey", apikey).
+                        header("solution1", solution1Header).
+                        contentType("application/json").
+                        body(towersSolution).
+                when().
+                        post("/towers").
+                then().
+                        log().all().
+                        statusCode(200).
+                extract()
+                        .response();
+        if (!towersPostResponse.asString().contains("Selenium")) {
+            specialistNumber = 587426;
+        } else if (!towersPostResponse.asString().contains("Postman")) {
+            specialistNumber = 627836;
+        } else if (!towersPostResponse.asString().contains("Cucumber")) {
+            specialistNumber = 7346337;
+        } else if (!towersPostResponse.asString().contains("Protractor")) {
+            specialistNumber = 527786;
+        } else {
+            specialistNumber = 32472;
+        }
+        solution2Header = towersPostResponse.header("solution2");
+        System.out.println("The solutionHeader for puzzle 1: " + solution2Header);
+    }
+
+
+    private void toolSupport() {
+        Response toolsupport =
+                given().
+                        log().all().
+                        header("apikey", apikey).
+                        header("solution2", solution2Header).
+                        pathParam("specialistNumber", specialistNumber).
+                when().
+                        get("/tool_support/call_specialist/{specialistNumber}").
+                then().
+                        log().all().
+                        statusCode(200).
+                extract()
+                        .response();
+
+        escapeCode = toolsupport.jsonPath().get("escapecode");
+        System.out.println("The escapecode is: " + escapeCode);
+    }
+
+    private void escapeTheRoom() {
+        given().
+                log().all().
+                header("apikey", apikey).
+                pathParam("escapecode", escapeCode).
+        when().
+                delete("/remove_lock/{escapecode}").
+        then().
+                log().all().
+                statusCode(200);
+    }
 
 }
---
+++ src/test/java/Towers.java
@@ -0,0 +1,55 @@
+public class Towers {
+    int aKerk;
+    int academieGebouw;
+    int martini;
+    int nieuweKerk;
+    int stJozefKerk;
+
+    public Towers(int aKerk, int academieGebouw, int martini, int nieuweKerk, int stJozefKerk) {
+        this.aKerk = aKerk;
+        this.academieGebouw = academieGebouw;
+        this.martini = martini;
+        this.nieuweKerk = nieuweKerk;
+        this.stJozefKerk = stJozefKerk;
+    }
+
+    public int getaKerk() {
+        return aKerk;
+    }
+
+    public void setaKerk(int aKerk) {
+        this.aKerk = aKerk;
+    }
+
+    public int getAcademieGebouw() {
+        return academieGebouw;
+    }
+
+    public void setAcademieGebouw(int academieGebouw) {
+        this.academieGebouw = academieGebouw;
+    }
+
+    public int getMartini() {
+        return martini;
+    }
+
+    public void setMartini(int martini) {
+        this.martini = martini;
+    }
+
+    public int getNieuweKerk() {
+        return nieuweKerk;
+    }
+
+    public void setNieuweKerk(int nieuweKerk) {
+        this.nieuweKerk = nieuweKerk;
+    }
+
+    public int getStJozefKerk() {
+        return stJozefKerk;
+    }
+
+    public void setStJozefKerk(int stJozefKerk) {
+        this.stJozefKerk = stJozefKerk;
+    }
+}
